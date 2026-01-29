59b5114a5af434b8d372c84cce59e74a76e88bf8
Siddhant Bhandare <sbhandare0342@gmail.com>
2026-01-30 01:08:31 +0530

    Solved problems .

---
+++ patterns/Pat2.java
@@ -0,0 +1,25 @@
+package patterns;
+
+public class Pat2 {
+
+    public static void main(String[] args) {
+
+        int n = 5; 
+        int start;
+
+        for (int i = 0; i < n; i++) {
+
+            if (i % 2 == 0)
+                start = 1;
+            else
+                start = 0;
+
+            for (int j = 0; j <= i; j++) {
+               
+                System.out.print(start);
+                 start = 1 - start;
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ patterns/pat4.java
@@ -0,0 +1,14 @@
+package patterns;
+
+public class pat4 {
+    public static void main(String[]args){
+        for (int i = 0 ; i < 5; i++){
+            for(char ch = 'A' ; ch <='A' +i ; ch++){
+                System.out.print(ch);
+            }
+            System.out.println();   
+        }
+
+    }
+    
+}
